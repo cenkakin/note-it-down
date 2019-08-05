93eec8e8c8095708b5aa4ae33089b33d397c1d58
cenkakin <cenk.akin91@gmail.com>
2019-08-05 20:43:11 +0200

    Websocket trial

---
+++ note/src/main/java/com/github/noteitdown/note/configuration/NoteConfiguration.java
@@ -0,0 +1,32 @@
+package com.github.noteitdown.note.configuration;
+
+import com.github.noteitdown.note.handler.NoteWebSocketHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.reactive.HandlerMapping;
+import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
+import org.springframework.web.reactive.socket.WebSocketHandler;
+import org.springframework.web.reactive.socket.server.support.WebSocketHandlerAdapter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class NoteConfiguration {
+
+	@Bean
+	public HandlerMapping handlerMapping() {
+		Map<String, WebSocketHandler> map = Map.of("/path", new NoteWebSocketHandler());
+
+		SimpleUrlHandlerMapping mapping = new SimpleUrlHandlerMapping();
+		mapping.setUrlMap(map);
+		mapping.setOrder(Ordered.HIGHEST_PRECEDENCE + 1);
+		return mapping;
+	}
+
+	@Bean
+	public WebSocketHandlerAdapter handlerAdapter() {
+		return new WebSocketHandlerAdapter();
+	}
+}
---
+++ note/src/main/java/com/github/noteitdown/note/handler/NoteWebSocketHandler.java
@@ -0,0 +1,47 @@
+package com.github.noteitdown.note.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.noteitdown.note.dto.NoteCommandDto;
+import org.springframework.web.reactive.socket.WebSocketHandler;
+import org.springframework.web.reactive.socket.WebSocketMessage;
+import org.springframework.web.reactive.socket.WebSocketSession;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.io.IOException;
+import java.time.Duration;
+
+import static java.util.UUID.randomUUID;
+
+public class NoteWebSocketHandler implements WebSocketHandler {
+
+	private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+	private Flux<String> eventFlux = Flux.generate(sink -> {
+		String event = randomUUID().toString();
+		sink.next(event);
+	});
+
+	private Flux<String> intervalFlux = Flux.interval(Duration.ofMillis(1000L))
+		.zipWith(eventFlux, (time, event) -> event);
+
+
+	@Override
+	public Mono<Void> handle(WebSocketSession session) {
+		Mono<Void> input = session.receive()
+			.map(WebSocketMessage::getPayloadAsText)
+			.map(this::toNoteCommandDto).then();
+		Mono<Void> output = session.send(intervalFlux
+			.map(session::textMessage));
+
+		return Mono.zip(input, output).then();
+	}
+
+	private NoteCommandDto toNoteCommandDto(String json) {
+		try {
+			return OBJECT_MAPPER.readValue(json, NoteCommandDto.class);
+		} catch (IOException e) {
+			throw new RuntimeException("Invalid JSON:" + json, e);
+		}
+	}
+}
